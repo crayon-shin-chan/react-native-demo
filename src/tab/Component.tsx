8efdfdef5d31b660d080e6d811a9a12ac1802ac7
chenzhenyuan@skyable.cn <1253665625@qq.com>
2021-04-23 23:37:15 +0800

    component list

--- src/tab/Component.tsx
+++ src/tab/Component.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { StyleSheet, Text, View, SafeAreaView, SectionList, StatusBar } from "react-native";
-import ActivityIndicatorDemo from 'component/system/ActivityIndicatorDemo';
 import { useNavigation } from "@react-navigation/core";
+import AntDesignIcon from 'react-native-vector-icons/AntDesign';
+import ActivityIndicatorDemo from 'component/system/ActivityIndicatorDemo';
 
 const DATA = [
   {
@@ -21,16 +22,30 @@ interface RenderItemProps {
 
 const Item: React.FC<ItemProps> = (props) =>{
     const navigation = useNavigation();
-   return (
+    return (
     <View style={styles.item}>
-        <Text style={styles.title} onPress={()=>navigation.navigate('CustomComponent',{ component: props.component })}>
+        <Text style={styles.title}>
             {props.name}
         </Text>
+        <AntDesignIcon name="rightcircleo" 
+            style={styles.icon} 
+            onPress={()=>navigation.navigate('CustomComponent',{ component: props.component })}
+        ></AntDesignIcon>
     </View>
     )
 };
 
 const renderItem:React.FC<RenderItemProps> = ({ item }) => <Item {...item} />
+
+interface SectionProps { 
+    section: { 
+        title:string
+    } 
+}
+
+const renderSectionHeader: React.FC<SectionProps> = ({ section: { title } }) => (
+    <Text style={styles.header}>{title}</Text>
+  )
 
 const ComponentTab = () => (
   <SafeAreaView style={styles.container}>
@@ -38,9 +53,7 @@ const ComponentTab = () => (
       sections={DATA}
       keyExtractor={(item, index) => item.name + index}
       renderItem={renderItem}
-      renderSectionHeader={({ section: { title } }) => (
-        <Text style={styles.header}>{title}</Text>
-      )}
+      renderSectionHeader={renderSectionHeader}
     />
   </SafeAreaView>
 );
@@ -51,17 +64,24 @@ const styles = StyleSheet.create({
     paddingTop: StatusBar.currentHeight,
     marginHorizontal: 16
   },
+  header: {
+    fontSize: 14,
+    backgroundColor: "#fff"
+  },
   item: {
     backgroundColor: "#f9c2ff",
     padding: 20,
-    marginVertical: 8
-  },
-  header: {
-    fontSize: 32,
-    backgroundColor: "#fff"
+    marginVertical: 8,
+    borderRadius: 5,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   title: {
-    fontSize: 24
+    fontSize: 12,
+  },
+  icon: {
+    fontSize: 26,
   }
 });
 
